009f3883026b8c7889fd23b74f34d2474d636bb7
Thabo <thabotitus@gmail.com>
2015-05-27 00:31:07 +0200

    building products page

--- js/manus.js
+++ js/manus.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
 
-	var allProducts = $.getJSON("js/products.json");
+	var shoes = products;
 
+	function buildProductPage(){
+		$.each(shoes, function( idx, val ){
+			$("#latest-items").append( buildItem( val ) );
+		});
+	}
 
-	//console.log( allProducts );
+	function buildItem( object ){
+		var item = "<div class='item one column'>" + 
+		"<div class='title'>" + object.title + "</div>" + 
+		"<p class='description'>" + object.description + "</p>" + 
+		"<span class='price'> R " + object.price + "</span>" +
+		"<img src='http://placehold.it/350x250' alt='" + object.image_alt + "'/>" +
+		"</div>";
+
+		return item;
+	}
+
+	// Run functions
+	buildProductPage();
 });
---
+++ js/products.js
@@ -0,0 +1,31 @@
+
+	var products = [
+		{
+			"title": "First",
+			"image_path" : "path/to/image",
+			"image_alt" : "Description of Shoe",
+			"description" : "Description of the shoe A",
+			"price" : "250",
+			"available" : true,
+			"tags" : [ "men", "sport" ]
+		},
+		{
+			"title": "Second",
+			"image_path" : "path/to/image",
+			"image_alt" : "Description of Shoe",
+			"description" : "Description of the shoe B",
+			"price" : "250",
+			"available" : true,
+			"tags" : [ "women", "sport" ]
+		},
+		{
+			"title": "Third",
+			"image_path" : "path/to/image",
+			"image_alt" : "Description of Shoe",
+			"description" : "Description of the shoe C",
+			"price" : "250",
+			"available" : false,
+			"tags" : [ "women", "sport" ]
+		}
+	]
+
