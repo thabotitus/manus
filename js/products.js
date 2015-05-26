7e6d1a2f300c51db6a25eb39bc9c7c67dd03f4c6
Thabo <thabotitus@gmail.com>
2015-05-26 15:56:00 +0200

    products.js

---
+++ js/products.js
@@ -0,0 +1,13 @@
+//Build Products Array
+function productList(){
+	var products = [
+		{
+			"title": "A shoe",
+			"image_path" : "path/to/image",
+			"image_alt" : "Description of Shoe",
+			"description" : "Description of the shoe",
+			"price" : "250",
+			"available": true
+		}
+	]
+}
