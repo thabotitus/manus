3054251c0cc93e8c704498a61f1c12a47aab243c
Thabo <thabotitus@gmail.com>
2015-05-27 11:00:10 +0200

    adding filter

--- js/manus.js
+++ js/manus.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
+	// Get shoes from array described in products.js
 	var shoes = products;
+	//var filteredArray = [];
 
-	function buildProductPage(){
-		$.each(shoes, function( idx, val ){
-			$("#latest-items").append( buildItem( idx,val ) );
+	// Generate products to showcase on products page
+	function buildProductPage( category ){
+		
+		$.each( buildFilteredArray( category ), function( idx,val ){
+			$("#products-list").append( buildItem( val ) );
 		});
 	}
 
-	function buildItem( id, object ){
+	// Build the individual product item to inject into DOM
+	function buildItem( object ){
+
 		// Create new jQuery object to work with
 		var item = $("<div></div>");
 
@@ -19,7 +25,9 @@ $(document).ready(function(){
 		var data = "<div class='title'>" + object.title + "</div>" + 
 		"<p class='description'>" + object.description + "</p>" + 
 		"<span class='price'> R " + object.price + "</span>" +
-		"<div class='tags'></div>"
+		"<div class='tags'></div>" +
+		"<span class'button'>More Info</span>" +
+		"<span class'button'>Add To Cart</span>" +
 		"<img src='http://placehold.it/350x250' alt='" + object.image_alt + "'/>" +
 		"</div>";
 
@@ -32,16 +40,49 @@ $(document).ready(function(){
 		return item;
 	}
 
-	function tagIterator( element, array ){
-		$.each(array, function( idx, val ){
-			element.append( "<span>" + val + "</span>" );
+	function tagIterator( element,array ){
+		$.each(array, function( idx,val ){
+			element.append( "<span class='tag'>" + val + "</span>" );
 		});
 	}
 
-	function filterbyCategory( e ){
-		//
+	function filterbyCategory(){
+		$(".category-link").click(function( e ){
+			// Prevent default behaviour from clicking
+			e.preventDefault();
+			var cat_filter;
+
+			// Rebuild the product page only products from the
+			// chose category
+			buildProductPage( cat_filter );
+
+		});
+	}
+
+	// Build array with only filtered results
+	function buildFilteredArray( filter ){
+		var filteredArray = [];
+		var count = 0;
+
+		$.each( shoes, function( idx,val ){
+			if ( val.category == filter ){
+				filteredArray[count] = val;
+				count += 1;
+			}
+		});
+
+		return filteredArray;
+	}
+
+
+	// Only adds up total for fun
+	function addToCart(){
+		$(".add-to-cart").click(function(){
+			//
+		});
 	}
 
 	// Run functions
-	buildProductPage();
+	buildProductPage("health");
+	filterbyCategory();
 });
--- js/products.js
+++ js/products.js
@@ -8,7 +8,7 @@ var products = [
 		"description" : "Description of the shoe A",
 		"price" : "250",
 		"available" : true,
-		"category" : "sport",
+		"category" : "safety",
 		"tags" : [ "men","sport","health" ]
 	},
 	{
@@ -30,6 +30,16 @@ var products = [
 		"available" : false,
 		"category" : "sport",
 		"tags" : [ "women","sport" ]
+	},
+	{
+		"title": "Fourth",
+		"image_path" : "path/to/image",
+		"image_alt" : "Description of Shoe",
+		"description" : "Description of the shoe D",
+		"price" : "250",
+		"available" : false,
+		"category" : "health",
+		"tags" : [ "women","sport","leisure" ]
 	}
 ]
 
